96408902aab027b4131331897fb52e11bd7e3c3f
Rahul <rahul@jaisimha.com>
2019-04-30 00:21:38 -0400

    Add final screens for chobani, schengen, ecru

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 function search(name) {
-    console.log(name);
-    if (name.toUpperCase() === "chobani".toUpperCase()) {
+    if (['CHOBANI'].indexOf(name.toUpperCase()) >= 0) {
         document.location.href = 'chobani.html';
+        return;
+    }
+    if (['ECRU', 'ECRU SHOES', 'ECRU SHOE'].indexOf(name.toUpperCase()) >= 0) {
+        document.location.href = 'ecru.html';
+        return;
+    }
+    if (['SCHENGEN', 'SCHENGEN VISA', 'NETHERLANDS VISA'].indexOf(name.toUpperCase()) >= 0) {
+        document.location.href = 'schengen.html';
+        return;
     }
 }
